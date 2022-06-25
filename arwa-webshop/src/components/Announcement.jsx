3539ce2c2126e1ffd836742278093a06d0b3a17a
Arwa Ashawy <arwa.ashawy@gmail.com>
2022-06-25 12:13:39 +0200

    created Announcement component

--- arwa-webshop/src/components/Announcement.jsx
+++ arwa-webshop/src/components/Announcement.jsx
@@ -1,10 +1,20 @@
+import styled from 'styled-components'
 
-
+const Container = styled.div`
+    height: 30px ;
+    background-color: black ;
+    color: white ;
+    display: flex;
+    justify-content:center ;
+    align-items:center ;
+    font-size: 12px ;
+    font-weight: 500 ;
+`
 const Announcement = () => {
   return (
-    <div>
-
-    </div>
+    <Container>
+        Free shipping from €30 and easy returns - Returns extended to 60 days
+    </Container>
   )
 }
 
