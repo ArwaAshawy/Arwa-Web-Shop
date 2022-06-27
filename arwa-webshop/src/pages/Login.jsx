0801a143d2ae59d92f10f61a5b38a5df505db1a5
Arwa Ashawy <arwa.ashawy@gmail.com>
2022-06-27 17:44:34 +0200

    created Login and register page

--- arwa-webshop/src/pages/Login.jsx
+++ arwa-webshop/src/pages/Login.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import styled from "styled-components"
+import {Link} from 'react-router-dom'
+
+const Container = styled.div`
+    width: 100vw;
+    height:85vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+        width: 40%;
+        padding: 20px;
+        background-color: white;
+`
+const Form = styled.form`
+        display: flex;
+        flex-direction:column ;
+        justify-content: center ;
+        align-items: center ;
+
+        flex-wrap: wrap;
+`
+const Title = styled.h1`
+        font-size: 24px;
+        font-weight: 400;
+        text-align:center ;
+        margin: 3% ;
+`
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+  border: none;
+  border-bottom: 1px solid black ;
+`;
+
+const Button = styled.button`
+  width: 43%;
+  border: none;
+  padding: 15px 20px;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  margin: 15px;
+`;
+
+// const Link = styled.a`
+//   margin: 5px 0px;
+//   font-size: 12px;
+//   text-decoration: underline;
+//   cursor: pointer;
+//   color: grey;
+// `;
+
+const NavLink = styled(Link)`
+    margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+  color: grey;
+`
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <>
+        
+        <Navbar/>
+        <Announcement/>
+          <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input placeholder ="username"/>
+                    <Input placeholder ="password"/>
+                    <Button>LOGIN</Button>
+                    <NavLink to="">DO NOT YOU REMEMBER THE PASSWORD?</NavLink>
+                    <NavLink to="/register">CREATE A NEW ACCOUNT</NavLink>
+                </Form>
+            </Wrapper>
+        </Container>
+        <Footer/>
+        </>
+    )
 }
 
 export default Login
--- arwa-webshop/src/pages/Register.jsx
+++ arwa-webshop/src/pages/Register.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
+import styled from "styled-components"
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+const Container = styled.div`
+    width: 100vw;
+    height:85vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Wrapper = styled.div`
+        width: 40%;
+        padding: 20px;
+        background-color: white;
+`
+const Form = styled.form`
+        display: flex;
+        flex-direction:column ;
+        justify-content: center ;
+        align-items: center ;
+
+        flex-wrap: wrap;
+`
+const Title = styled.h1`
+        font-size: 24px;
+        font-weight: 400;
+        text-align:center ;
+        margin: 3% ;
+`
+const Text = styled.p`
+       font-size: 12px;
+        margin: 20px 0px;
+        width: 45% ;
+        text-align:center ;
+`
+const Input =  styled.input`   
+        /* flex: 1; */
+        min-width: 40%;
+        margin: 20px 10px 0 0;
+        padding: 10px;
+        border: none;
+        border-bottom: black 1px solid;
+`
+
+const Agreement = styled.span`
+        font-size: 12px;
+        margin: 20px 0px;
+        width: 45% ;
+`
+
+const Button = styled.button`
+        width: 45%;
+        border: none;
+        padding: 15px 20px;
+        background-color: black;
+        color: white;
+        cursor: pointer;
+`
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    return (
+      <>
+        <Navbar/>
+        <Announcement/>
+        <Container>
+            <Wrapper>
+                <Title>CREATE AN ACCOUNT</Title>
+               
+                <Form>
+                <Text>Enjoy discounts and unique experiences in stores and online shopping</Text>
+                    <Input placeholder ="First name"/>
+                    <Input placeholder ="Last name"/>
+                    <Input placeholder ="Email"/>
+                    <Input placeholder ="Mobile number"/>
+                    <Input placeholder ="Username"/>
+                    <Input placeholder ="Password"/>
+                    <Input placeholder ="Confirm password"/>
+                    <Agreement>
+                    By creating an account, I consent to the processing of my personal
+                    data in accordance with the <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>Create</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+        <Footer/>
+      </>
+        
+    )
 }
 
 export default Register
